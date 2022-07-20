4b1f7784c2280c7d2db8a27676b4c10f973bf195
Mohammed <mhmdtajeldin@gmail.com>
2022-07-20 08:04:49 +0200

    with bug in the page overflow

--- lib/main.dart
+++ lib/main.dart
@@ -39,69 +39,105 @@ class _HomeState extends State<Home> {
           padding: const EdgeInsets.only(left: 40, right: 40),
           child: Form(
             key: formKey, //key for form
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: height * 0.04),
-                const Text(
-                  "Here to Get",
-                  style: TextStyle(fontSize: 30, color: Color(0xFF363f93)),
-                ),
-                const Text(
-                  "Welcomed !",
-                  style: TextStyle(fontSize: 30, color: Color(0xFF363f93)),
-                ),
-                SizedBox(
-                  height: height * 0.05,
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: "Enter Your Name",
+            child: Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: height * 0.04),
+                  const Text(
+                    "Here to Get",
+                    style: TextStyle(fontSize: 30, color: Color(0xFF363f93)),
                   ),
-                  validator: (value) {
-                    if (value!.isEmpty ||
-                        !RegExp(r'^[a-z A-Z]+$').hasMatch(value!)) {
-                      return "Enter your name properly";
-                    } else {
-                      return null;
-                    }
-                  },
-                ),
-                SizedBox(
-                  height: height * 0.05,
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: "Enter Your Phone",
+                  const Text(
+                    "Welcomed !",
+                    style: TextStyle(fontSize: 30, color: Color(0xFF363f93)),
                   ),
-                  validator: (value) {
-                    if (value!.isEmpty ||
-                        !RegExp(r'^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]+$')
-                            .hasMatch(value!)) {
-                      return "Enter your phone properly.";
-                    } else {
-                      return null;
-                    }
-                  },
-                ),
-                SizedBox(
-                  height: height * 0.05,
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: "Enter Your Email",
+                  SizedBox(
+                    height: height * 0.05,
                   ),
-                  validator: (value) {
-                    if (value!.isEmpty ||
-                        !RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}')
-                            .hasMatch(value!)) {
-                      return "Enter your email properly";
-                    } else {
-                      return null;
-                    }
-                  },
-                ),
-              ],
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: "Enter Your Name",
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty ||
+                          !RegExp(r'^[a-z A-Z]+$').hasMatch(value!)) {
+                        return "Enter your name properly";
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: height * 0.05,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: "Enter Your Phone",
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty ||
+                          !RegExp(r'^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]+$')
+                              .hasMatch(value!)) {
+                        return "Enter your phone properly.";
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: height * 0.05,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: "Enter Your Email",
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty ||
+                          !RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}')
+                              .hasMatch(value!)) {
+                        return "Enter your email properly";
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: height * 0.05,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      const Text(
+                        "Sign up",
+                        style: TextStyle(
+                            fontSize: 22,
+                            color: Color.fromARGB(255, 3, 75, 134)),
+                      ),
+                      NeumorphicButton(
+                        margin: const EdgeInsets.only(top: 12),
+                        onPressed: () {
+                          if (formKey.currentState!.validate()) {
+                            const snackBar = SnackBar(
+                              content: Text('Submit Form'),
+                            );
+                            _scaffoldKey.currentState!.showSnackBar(snackBar);
+                          }
+                        },
+                        style: const NeumorphicStyle(
+                          shape: NeumorphicShape.flat,
+                          boxShape: NeumorphicBoxShape.circle(),
+                        ),
+                        padding: const EdgeInsets.all(12.0),
+                        child: const Icon(
+                          Icons.arrow_right_alt,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ));
